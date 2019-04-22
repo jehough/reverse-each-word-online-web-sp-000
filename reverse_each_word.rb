d786e6d62c8d607ad99af3d691dd3822433bedfd
Jim Hough <teachlife@outlook.com>
2019-04-22 21:38:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   arr = phrase.split(" ")
-  reversed_array = arr.map do |word|
+  reversed_array = arr.collect do |word|
     word.reverse
   end
   reversed_array.join(" ")
